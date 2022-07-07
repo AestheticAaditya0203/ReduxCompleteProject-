9e7d92f9ca59a386c65cf2d65ee7051d2d43f9a8
AestheticAaditya0203 <103627563+AestheticAaditya0203@users.noreply.github.com>
2022-07-07 11:45:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Provider } from 'react-redux'
+import store from './store'
+import UserData from "./components/UserData";
+import Navbar from "./components/Navbar";
+import IncDec from "./components/IncDec";
+import {Routes , Route} from 'react-router-dom'
+import Home from "./components/Home";
+import ViewDetails from "./components/ViewDetails";
+//import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+
+
+const App = () => {
+ 
+    return(
+        <>
+        <div>
+        <Navbar />
+        <Provider store={store}>
+        <Routes>
+              
+              <Route path="/home" element={<Home />} />
+              <Route path="/userdata" element={<UserData />} />
+              <Route path="/viewdetails/:id" element={<ViewDetails />} />
+              <Route path="/incdec" element={<IncDec />} />
+               
+        </Routes>
+        </Provider>
+        </div>
+        </>
+    )
+}
+
+export default App;
